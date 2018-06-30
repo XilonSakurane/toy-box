90a09cd909ad68336d53b37949cb01adcd1bf4f3
XilonSakurane <epsilon_xjmptw0@yahoo.co.jp>
2018-06-30 23:55:02 +0900

    リアクションをつける機能を追加("smile1","smile2","smile3","smile4"に対応)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import discord
+from discord.utils import get
 import asyncio
 import pdb
 import hamutaro
 import configparser
+import random
 
 inifile = configparser.ConfigParser()
 client = discord.Client()
@@ -22,6 +24,20 @@ async def on_message(msg):
         if "ハム太郎" in msg.content and "お前もそう思う" in msg.content:
             m = hamutaro.affirmation(msg, client)
             await client.send_message(msg.channel, m)
+
+        if "？？？？？" in msg.content or "?????" in msg.content:
+            emoji1 = "smile" + str(random.randrange(4) + 1)
+            emoji2 = "smile" + str(random.randrange(4) + 1)
+            while emoji1 == emoji2:
+                emoji2 = "smile" + str(random.randrange(4) + 1)
+
+            e1 = get(client.get_all_emojis(), name = emoji1)
+            e2 = get(client.get_all_emojis(), name = emoji2)
+            await client.add_reaction(msg, e1)
+            await client.add_reaction(msg, e2)
+            print("AddReaction:" + emoji1 + ", " + emoji2)
+
+
 
 ## コンフィグ情報の読み込み ##
 
